99f9dad88002cf402221bde58a32edad9469a3be
Subham <subhamsingh5638@gmail.com>
2024-03-08 23:52:02 +0530

    completed expenditure-analysis assignment

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -7,9 +7,26 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-expenditure-analysis`
 */
+ 
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let map = new Map();
+  for(let i= 0; i < transactions.length; i++){
+    let key = transactions[i].category;
+    if(map.has(key)){
+      map.set(key, map.get(key) + transactions[i].price);
+    }else{
+      map.set(key, transactions[i].price);
+    }
+  }
+  let ans = [] ;
+  
+  for(let [key, value] of map){
+    ans.push({category: key, totalSpent : value} );
+  }
+  
+  return ans;
 }
 
+  
 module.exports = calculateTotalSpentByCategory;
